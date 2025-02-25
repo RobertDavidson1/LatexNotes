f158ba485b645b37dc3022fa076bee5c32e0b3e2
Robert Davidson <118013926+RobertDavidson1@users.noreply.github.com>
2025-02-25 22:59:29 +0000

    Refine automated commit to add only specific file types

--- auto_git_sync.sh
+++ auto_git_sync.sh
@@ -6,9 +6,8 @@ cd ~/Desktop/College/LatexNotes || exit
 # Pull latest changes
 git pull origin main
 
-# Add all changes
-git add .
-
+# Add only .tex and .sty files from src and .pdf files from src/pdf
+git add src/*.tex src/*.sty src/pdf/*.pd
 # Commit with current date
 git commit -m "Automated commit: Notes updated on $(date '+%Y-%m-%d %H:%M:%S')"
 
