ebaa0a5f9dd8909920361304a8108f7094fc2f15
Robert Davidson <118013926+RobertDavidson1@users.noreply.github.com>
2025-03-19 07:33:52 +0000

    Add automated script for syncing LaTeX notes with remote repository

---
+++ auto_git_sync.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Navigate to the repo
+cd ~/Desktop/College/LatexNotes || exit
+
+# Pull latest changes
+git pull origin main
+
+# Add only .tex and from src and .pdf files from src/pdf
+git add src/*.tex  src/pdf/*.pdf
+# Commit with current date
+git commit -m "Automated commit: Notes updated on $(date '+%Y-%m-%d %H:%M:%S')"
+
+# Push to remote repo
+git push origin main
