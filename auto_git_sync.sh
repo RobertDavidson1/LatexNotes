fbfecd96d97a2e98d6972379f684c03204226454
Robert Davidson <118013926+RobertDavidson1@users.noreply.github.com>
2025-04-03 17:40:34 +0100

    Update automated commit message format for notes

--- auto_git_sync.sh
+++ auto_git_sync.sh
@@ -9,7 +9,7 @@ git pull origin main
 # Add only .tex and from src and .pdf files from src/pdf
 git add src/*.tex  pdf/*.pdf
 # Commit with current date
-git commit -m "Automated commit: Notes updated on $(date '+%Y-%m-%d %H:%M:%S')"
+git commit -m "Notes updated on $(date -d 'now' '+%H:%M %d-%m-%Y') (Automated commit)"
 
 # Push to remote repo
 git push origin main
